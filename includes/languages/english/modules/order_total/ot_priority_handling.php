700d1b9e5f6e47d972da9d9963419de1e02d01be
lat9 <lat9@vinosdefrutastropicales.com>
2018-10-12 15:16:08 -0400

    #2: Capture "forgotten" language updates

--- includes/languages/english/modules/order_total/ot_priority_handling.php
+++ includes/languages/english/modules/order_total/ot_priority_handling.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  $Id: ot_insurance.php,v 1.1 2006/02/288  Exp $
+  $Id: ot_insurance.php,v 1.3.0 20181012 lat9
 
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
@@ -10,9 +10,10 @@
   Released under the GNU General Public License
 */
 
-  define('MODULE_ORDER_TOTAL_PRIORITY_HANDLING_TITLE', 'Priority Handling');
-  define('MODULE_ORDER_TOTAL_PRIORITY_HANDLING_DESCRIPTION', 'Priority Handling');
-  define('MODULE_ORDER_TOTAL_PRIORITY_HANDLING_TEXT_DESCR', 'Rush my order!  Select this optional handling by clicking on the box below next to Add Priority Handling.  A priority handling charge will be added to your total after you click continue.');
-  define('MODULE_ORDER_TOTAL_PRIORITY_HANDLING_TEXT_ENTER_CODE', 'Add Priority Handling:');
+define('MODULE_ORDER_TOTAL_PRIORITY_HANDLING_TITLE', 'Priority Handling');
+define('MODULE_ORDER_TOTAL_PRIORITY_HANDLING_DESCRIPTION', 'Priority Handling');
+define('MODULE_ORDER_TOTAL_PRIORITY_HANDLING_TEXT_DESCR', 'Rush my order!  Select this optional handling by clicking on the box below next to Add Priority Handling.  A priority handling charge will be added to your total after you click continue.');
+define('MODULE_ORDER_TOTAL_PRIORITY_HANDLING_TEXT_ENTER_CODE', 'Add Priority Handling:');
 
-?>
+define('MODULE_ORDER_TOTAL_PRIORITY_HANDLING_YES', 'Yes');
+define('MODULE_ORDER_TOTAL_PRIORITY_HANDLING_NO', 'No');
