725fc1772f279301c29f41befd37f55994e9ac23
carolinalaser <54193897+carolinalaser@users.noreply.github.com>
2024-12-14 14:18:28 -0500

    Updates 2024-12-14
    Updates for PHP 8+.  Tested in ZC 2.01 and 2.10, PHP 8.2 and 8.3.

---
+++ includes/languages/english/modules/order_total/lang.ot_priority_handling.php
@@ -0,0 +1,12 @@
+<?php
+$define = [
+    'MODULE_ORDER_TOTAL_PRIORITY_HANDLING_TITLE' => 'Priority Handling',
+    'MODULE_ORDER_TOTAL_PRIORITY_HANDLING_DESCRIPTION' => 'Priority Handling',
+    'MODULE_ORDER_TOTAL_PRIORITY_HANDLING_TEXT_DESCR' => 'Rush my order! Select this optional handling by clicking on the box below next to Add Priority Handling. A priority handling charge will be added to your total after you click continue.',
+    'MODULE_ORDER_TOTAL_PRIORITY_HANDLING_TEXT_ENTER_CODE' => 'Add Priority Handling:',
+    'MODULE_ORDER_TOTAL_PRIORITY_HANDLING_YES' => 'Yes',
+    'MODULE_ORDER_TOTAL_PRIORITY_HANDLING_NO' => 'No',
+];
+
+return $define;
+?>
